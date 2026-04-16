7b755a24693e8c6d6e36ad9aa3e32186e0cd755e
collin-beck3 <collin.beck3@gmail.com>
2026-04-16 17:40:57 -0400

    dice created with n number of dice to roll, and a simulation that rolls the dice.

--- java/src/main/java/Dice.java
+++ java/src/main/java/Dice.java
@@ -1,4 +1,24 @@
+import java.util.Random;
+
 public class Dice {
+
+    private int sides;
+    private int numberOfDice;           // stores the number of dice to be rolled
+    private Random random;
+
+    public Dice(int sides, int numberOfDice) {
+        this.sides = sides;
+        this.numberOfDice = numberOfDice;
+        this.random = new Random();
+    }
+
+    public int roll() {
+        int total = 0;
+        for (int i = 0; i < numberOfDice; i++) {            // loop to roll each die one by one 
+            total += random.nextInt(sides) + 1;             // adds each roll to the running total
+        }
+        return total;
+    }
 
 
 }
--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -1,5 +1,14 @@
 public class Simulation {
 
+public static void main(String[] args) {
+    Dice dice = new Dice(6, 2); // Create a dice object with 6 sides and n dice to roll
 
+    System.out.println(); 
+    System.out.println("Rolling the dice...");
+    for (int i = 0; i < 10; i++) { 
+        int result = dice.roll();
+        System.out.println("Roll " + (i + 1) + ": " + " Total: " + result);
+    }
+}
 
 }

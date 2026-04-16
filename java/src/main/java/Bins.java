f8cde13ccd82fd5d6ae84210b10e8ea2ad201db2
collin-beck3 <collin.beck3@gmail.com>
2026-04-16 18:00:24 -0400

    added bins and simulation for total count

--- java/src/main/java/Bins.java
+++ java/src/main/java/Bins.java
@@ -1,4 +1,22 @@
 
 public class Bins {
+    private int[] counts; // Array to store the count of each possible outcome
 
+    // Constructor to initialize the bins based on the number of dice and sides
+    public Bins(int size) {
+        this.counts = new int[size];
+    }
+    // Method to increment the count for a specific outcome
+    public void increment(int value) { 
+        counts[value]++; 
+    }
+
+    // Method to get the count for a specific outcome
+    public int getCount(int value) { 
+        return counts[value];
+    }
+
+    public int getSize() {
+        return counts.length;
+    }
 }
--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -1,13 +1,20 @@
 public class Simulation {
 
 public static void main(String[] args) {
-    Dice dice = new Dice(6, 2); // Create a dice object with 6 sides and n dice to roll
+    Dice dice = new Dice(6, 2);     // Create a dice object with 6 sides and n dice to roll
+    Bins bins = new Bins(13); // Create bins to store the counts of each possible outcome
 
     System.out.println(); 
     System.out.println("Rolling the dice...");
-    for (int i = 0; i < 10; i++) { 
+    for (int i = 0; i < 10000; i++) {                      // loop to roll the dice n times
         int result = dice.roll();
-        System.out.println("Roll " + (i + 1) + ": " + " Total: " + result);
+        bins.increment(result); // Increment the count for the rolled result
+        System.out.println("Roll " + (i + 1) + ": " + " Total: " + result);         //print result 
+    }
+
+    System.out.println("\nResults:");
+    for (int i = 2; i<= 12; i++) { 
+        System.out.println("Total " + i + ": " + bins.getCount(i) + " times");  
     }
 }
 
